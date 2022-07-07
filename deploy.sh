02075c0b93d0be4207675fe860330d6617f030c1
김경준 <kouz@woowahan.com>
2022-07-07 19:15:00 +0900

    chore: apply beta profile

--- deploy.sh
+++ deploy.sh
@@ -17,4 +17,4 @@ else
 fi
 
 echo "> deploy $JAR_PATH"
-nohup java -jar $JAR_PATH > $REPOSITORY/nohup.out 2>&1 &
+nohup java -jar -Dspring.profiles.active=beta "$JAR_PATH"  > $REPOSITORY/nohup.out 2>&1 &
