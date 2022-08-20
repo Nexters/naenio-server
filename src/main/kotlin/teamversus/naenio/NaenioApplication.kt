79d673d02ccbabf0c412e0b561666a1d556ff8c9
김경준 <kouz@woowahan.com>
2022-08-20 10:03:45 +0900

    chore: set time zone

--- src/main/kotlin/teamversus/naenio/NaenioApplication.kt
+++ src/main/kotlin/teamversus/naenio/NaenioApplication.kt
@@ -2,9 +2,17 @@ package teamversus.naenio
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.util.*
+import javax.annotation.PostConstruct
+
 
 @SpringBootApplication
-class NaenioApplication
+class NaenioApplication {
+    @PostConstruct
+    fun setTimeZone() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"))
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<NaenioApplication>(*args)
